6a1d06874117c3c117d6079d86405dcefde32b64
Kritarth Verma <kritarth4444@gmail.com>
2024-09-13 21:15:55 +0530

    Configured socket.io

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,30 +1,45 @@
 import React, { useEffect, useRef, useState } from 'react'
+import toast from 'react-hot-toast'
 import Editor from "../components/Editor"
 import Client from "../components/Client"
 import { initSocket } from '../socket';
 import ACTIONS from '../Actions';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate, Navigate, useParams } from 'react-router-dom';
 
 const EditorPage = () => {
   const socketRef = useRef(null);
   const location = useLocation();
-
+  const reactNavigator = useNavigate();
+  const {roomId} = useParams();
+  const [clients,setClients] = useState([]);
+ 
   useEffect(()=>{
     const init = async ()=>{
       socketRef.current = await initSocket();
-      // socketRef.current.emit(ACTIONS.JOIN,{
-      //   roomId,
-      //   username:location.state?.username,
-      // });
+      socketRef.current.on('connect_error',(err)=>handleErrors(err));
+      socketRef.current.on('connect_failed',(err)=>handleErrors(err));
+      function handleErrors(e){
+        console.log('socket error', e);
+        toast.error('Socket connection failed, try again later.')
+        reactNavigator('/');
+      }
+      socketRef.current.emit(ACTIONS.JOIN,{
+        roomId,
+        username:location.state?.username,
+      });
+      socketRef.current.on(ACTIONS.JOINED,({clients,username,socketId})=>{
+        if(username !== location.state?.username){
+          toast.success(`${username} joined the room`);
+        }
+        setClients(clients);
+      });
     }
     init();
   },[])
 
-  const [clients,setClients] = useState([
-    {socketId : 1,username:"Kritarth V"},
-    {socketId: 2,username:"John Doe"},
-    {socketId: 3,username:"Jane Doe"},
-  ]);
+  if(!location.state){
+    return <Navigate to='/'/>
+  }
   return (
     <div className='mainWrap'>
       <div className='aside'>
